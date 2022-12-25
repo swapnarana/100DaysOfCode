358c423429ebfe1d68a4a7addb4929d67d4e0609
Swapna Rana <45589959+swapnarana@users.noreply.github.com>
2022-12-25 22:29:59 +0530

    Create Day1:BandNameGenerator.py

---
+++ Day1:BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Swap's Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your first pet?\n")
+print("Your band name is: " + city + " " + pet)
